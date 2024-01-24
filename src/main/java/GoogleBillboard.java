19cdf88a8701bcf9897778123d9303e45166113b
Griffin-623 <79292572+Griffin-623@users.noreply.github.com>
2024-01-23 21:06:51 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,36 @@
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 
 
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
-	}
-	
 
-	//Finish this function
-	public boolean isPrime(double d){
-	}
+boolean check = false;
+public void setup()  
+{            
+  //String digits = e.substring(2,12);
+  double dNum = 0;
+  //System.out.println(dNum);  //displays 7.182818284E9
+  //.System.out.println(isPrime(dNum));
+  for(int i = 2;check==false;i++){
+    dNum = Double.parseDouble(e.substring(i,i+10));
+    if(isPrime(dNum)){
+      System.out.println(dNum);
+      
+      check = true;
+    }
+  }
+}  
 
+
+
+ public boolean isPrime(double num){
+  if(num<2){
+    return false;
+  }else{
+    for(int i =2;i<=Math.sqrt(num);i++){
+      if(num%i==0){
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
